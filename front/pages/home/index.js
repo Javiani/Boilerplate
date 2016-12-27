6979a7213e653638307d2b2a547049ed88c1d0e4
Eduardo Ottaviani Aragão <earagao@fbiz.com.br>
2016-12-27 15:00:24 -0200

    Removing unused logs

--- front/pages/home/index.js
+++ front/pages/home/index.js
@@ -1,22 +1,14 @@
-import jails from 'jails'
-import counter from 'components/counter'
-import thirdparty from 'components/third-party'
-import store from 'components/counter/store'
+import jails	 	from 'jails'
+import counter 		from 'components/counter'
+import thirdparty 	from 'components/third-party'
+import store 		from 'components/counter/store'
 
 jails('counter', counter)
 jails('third-party', thirdparty)
 
 // Just logging stuff, none of the code bellow is necessary.
-
 console.log('You\'re at Home!')
-
-const log = ( script, tag )=>{
-	console.info(`[third-party/${script.name}] is loaded`)
-}
 
 store.subscribe( state => {
 	console.info( 'COUNTER.Store =>', state )
 })
-
-jails.subscribe('third-party:facebook', log)
-jails.subscribe('third-party:analytics', log)
