6ddaf9eb4540f349581346371ca193590254dc6e
Eduardo Ottaviani Aragão <earagao@fbiz.com.br>
2016-12-28 21:21:04 -0200

    Adding .babelrc for es2015 usage

--- front/components/counter/template.jsx
+++ front/components/counter/template.jsx
@@ -3,7 +3,7 @@ import vdom from 'jails-modules/vdom'
 export default vdom(( state )=>{
 	return (
 		<div class="holder">
-			<input  type="number" value={ String(state.counter) } readonly />
+			<input type="number" value={ String(state.counter) } readonly />
 			<button type="button" value="SUBTRACT">-</button>
 			<button type="button" value="ADD">+</button>
 		</div>
