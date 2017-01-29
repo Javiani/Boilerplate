04021329ae604d0ac5b27f47a0d79aae4e78ab42
Eduardo Ottaviani Aragão <earagao@fbiz.com.br>
2017-01-29 18:57:38 -0200

    Upgrading webpack and jails

--- front/main.js
+++ front/main.js
@@ -1,14 +1,12 @@
 import jails from 'jails'
 import scriptjs from 'scriptjs'
 import logger from 'jails-modules/logger'
-import adapter from 'modules/jquery.adapter'
 
 let mainscript 	= document.getElementById('main-script')
-let app = mainscript.getAttribute('data-application')
-let jquery = '//code.jquery.com/jquery-3.1.1.min.js'
+let app 		= mainscript.getAttribute('data-application')
+let jquery 		= '//code.jquery.com/jquery-3.1.1.min.js'
 
-scriptjs([ app, jquery ], ()=>{
-	logger()
-	jails.events = adapter( jQuery )
+scriptjs([ jquery, app ], ()=>{
+	logger( jails )
 	jails.start()
 })
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,42 +1,44 @@
 import path from 'path'
 import glob from 'glob'
 import webpack from 'webpack'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 let optimize = webpack.optimize
 let dev	 	 = process.env.NODE_ENV != 'production'
 
 let config = {
 	js	: './front',
-	dist: `${__dirname}/dist/js/`,
-	publicPath :'./dist/js/'
+	dist: `${__dirname}/dist/`,
+	publicPath :'./dist/'
 }
 
 export default {
 
-	entry : glob.sync( `${config.js}/pages/**/*.js` ).reduce( entries, {
+	entry : glob.sync( `${config.js}/pages/**/*{.js,.styl}` ).reduce( entries, {
 		main:[ `${config.js}/main` ]
 	}),
 
 	output: {
 		path: config.dist,
-		filename: '[name].min.js',
+		filename: '[name]/index.js',
 		publicPath: config.publicPath
 	},
 
 	resolve:{
-		root  :[ path.resolve(config.js) ],
+		root  :[ path.resolve(config.js), path.resolve(config.dist) ],
 		alias :{
-			jails :'jails-js/source/jails'
+			jails :'jails-js/dist/jails.legacy'
 		}
 	},
 
 	plugins :[
-		new optimize.CommonsChunkPlugin('main', 'main.min.js')
+		new optimize.CommonsChunkPlugin('main', 'main.js')
 	].concat(
 		dev? [] :new optimize.UglifyJsPlugin({
 			compress :{ warnings:false },
 			minimize :true}
-		)
+		),
+		new ExtractTextPlugin('[name]/index.css', {allChunks: false})
 	),
 
 	module: {
@@ -45,21 +47,22 @@ export default {
 			test: /\.js|\.jsx$/,
 			exclude: /node_modules/,
 			query:{
-				presets: [ 'es2015' ],
-				plugins: [[ 'transform-react-jsx', { 'pragma': 'vdom.h' } ]]
+				presets:['es2015'],
+				plugins:[['transform-react-jsx', { 'pragma': 'vdom.h'}]]
 			}
+		},
+		{
+			test:   /\.styl$/,
+			loader: ExtractTextPlugin.extract('css-loader!stylus-loader?paths[]=node_modules&paths[]=front')
 		}]
 	}
 }
 
-function entries( acc, file ){
+function entries(acc, file){
 
-	let filename  = path.basename(file, '.js'),
-		directory = path.dirname(file),
-		dir = directory.split(/\//).pop()
-
-	filename = dir == 'apps' ? filename : `${dir}/${filename}`
-	acc[filename] = `./${file}`
+	let filename = path.basename(file)
+	let dir 	 = path.dirname(file).split(/\//).pop()
+	acc[dir] 	 = (acc[dir] || []).concat(file)
 
 	return acc
 }
